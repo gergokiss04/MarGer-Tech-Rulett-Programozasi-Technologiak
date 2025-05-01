1c4f44f851f99c2e3a705d8303d2b0e64019ed46
gergokiss04 <94444225+gergokiss04@users.noreply.github.com>
2025-05-01 12:09:32 +0200

    Kezdő Adatbázis kapcsolat

--- Rulett-JavaFX/src/main/java/module-info.java
+++ Rulett-JavaFX/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.rulettjavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.rulettjavafx to javafx.fxml;
--- Rulett-JavaFX/src/main/java/org/example/rulettjavafx/HelloController.java
+++ Rulett-JavaFX/src/main/java/org/example/rulettjavafx/HelloController.java
@@ -4,11 +4,26 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
     @FXML
     private Label welcomeText;
 
+
+    public void initialize() {
+        welcomeText.setText("Köszöntjük a Roulett birodalomban!");
+    }
+
+
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onRegisterButtonClick() {
+        System.out.println("Regisztráció...");
+        // Ide jöhet a regisztrációs logika, pl. egy új ablak nyitása
+    }
+
+
+    @FXML
+    private void onLoginButtonClick() {
+        System.out.println("Bejelentkezés...");
+        // Ide jöhet a bejelentkezési logika, pl. egy új ablak nyitása
     }
 }
